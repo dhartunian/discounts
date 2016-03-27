da3129f41d3dd71b41cedc952be2cc3816f9a060
David Hartunian <david@musicismath.net>
2016-03-27 15:39:43 -0400

    Add types and sample data from problem statement
    - Had to make a few tweaks to please the compiler.
      They are noted in comments.

--- src/Discounts.hs
+++ src/Discounts.hs
@@ -1,4 +1,23 @@
 module Discounts where
 
+import Data.Text
+
+data LineItem = LineItem { lineItemProductId :: Int
+                         , lineItemQuantity :: Int }
+
+data Order = Order { orderLineItems :: [LineItem]
+                   , orderDiscountId :: Maybe Int }
+
+data Mode = All | Some [Int]
+
+-- changed type "Variant" of discountVariant to "Mode" since that's
+-- what was defined above
+data Discount = Discount { discountPercentage :: Int
+                         , discountVariant :: Mode
+                         , discountCopies :: Maybe Int}
+
+data Product = Product { productName :: Text
+                       , productPrice :: Int }
+
 discount :: Int
 discount = 0
--- test/tests.hs
+++ test/tests.hs
@@ -2,6 +2,25 @@ module Main where
 
 import Discounts
 import Test.Hspec
+import Data.Text
+import Data.Map
+
+sampleOrder1 :: Order
+sampleOrder1 = Order [LineItem 1 1] Nothing
+
+sampleOrder2 :: Order
+sampleOrder2 = Order [LineItem 1 1] (Just 1)
+
+-- added () around Some [1]
+discountDb :: Map Int Discount
+discountDb = Data.Map.fromList [ (1, Discount 50 All (Just 1))
+                               , (2, Discount 75 (Some [1]) Nothing)]
+
+-- wrapped strings in pack to turn into Text
+productDb :: Map Int Product
+productDb = Data.Map.fromList [ (1, Product (pack "Black Jacobins") 2000)
+                              , (2, Product (pack "Shock Doctrine") 1500)]
+
 
 main :: IO ()
 main = hspec $ do
