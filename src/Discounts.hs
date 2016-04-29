06f767c925c3e789213819e3c960c177e44e86fd
David Hartunian <davidh@knewton.com>
2016-04-29 17:20:34 -0400

    WIP refactoring

--- src/Discounts.hs
+++ src/Discounts.hs
@@ -28,17 +28,11 @@ type Cost = Int
 
 -- computes cost of an item given a product db in cents
 itemCost :: ProductDatabase -> LineItem -> Maybe Cost
-itemCost db (LineItem pid quantity) =
-  case Data.Map.lookup pid db of
-    Just (Product _ cost) -> Just (cost * quantity)
-    Nothing -> Nothing
+itemCost db (LineItem pid quantity) = (*) quantity <$> productPrice <$> (flip Data.Map.lookup db) pid
 
 -- computes cost of an item with discounts applied
 discountedItemCost :: ProductDatabase -> Discount -> LineItem -> Maybe Cost
-discountedItemCost pdb discount li =
-  case itemCost pdb li of
-    Just cost -> Just (applyDiscountToCost li discount cost)
-    Nothing -> Nothing
+discountedItemCost pdb discount li = applyDiscountToCost li discount <$> itemCost pdb li
 
 -- applies discount percentage to cost and rounds down result
 applyDiscount :: Cost -> Int -> Cost
@@ -63,7 +57,7 @@ applyDiscountToCost li discount =
 
 orderCost :: ProductDatabase -> DiscountDatabase -> Order -> Cost
 orderCost pdb ddb (Order items maybeDiscount) =
-  Prelude.foldr (+) 0 $ catMaybes $ Prelude.map (discountedItemCost pdb discount) items
-  where discount = case maybeDiscount >>= (flip Data.Map.lookup $ ddb) of
-          Just d -> d
-          Nothing -> (Discount 100 All Nothing)
+  Prelude.foldr (+) 0 $ catMaybes $ flip Prelude.map items $ priceFunction $ (flip Data.Map.lookup ddb) <$> maybeDiscount
+  where priceFunction disc = case disc of
+          Just d -> discountedItemCost pdb d
+          Nothing -> itemCost pdb
